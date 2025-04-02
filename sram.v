e5f5ac324307def47193e34c38e4f1c317ba8fef
Maheshwari1912 <mahiratthapuram@gmail.com>
2025-04-02 22:03:35 +0530

    Create sram.v

---
+++ sram.v
@@ -0,0 +1,16 @@
+module sync_sram(
+  input clk,
+  input [3:0] addr,
+  input [7:0] data_in,
+  input wr,
+  output reg [7:0] data_out);
+  reg [7:0] mem [15:0];
+  always@(posedge clk) begin
+    if(wr) begin
+      mem[addr]=data_in;
+    end
+    else begin
+      data_out = mem[addr];
+    end
+  end
+endmodule
